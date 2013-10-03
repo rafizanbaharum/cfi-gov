29cec57d1da08bb54ca649e96cbe95683ccc76af
Rafizan Uda Baharum <rafizan.baharum@gmail.com>
2013-10-03 19:16:59 +0000

    Added data

--- src/main/java/net/canang/cfi/biz/bm/manager/workflow/ControlTask.java
+++ src/main/java/net/canang/cfi/biz/bm/manager/workflow/ControlTask.java
@@ -1,8 +1,19 @@
 package net.canang.cfi.biz.bm.manager.workflow;
 
+import net.canang.cfi.biz.integration.activiti.TaskAdapter;
+import net.canang.cfi.core.bm.model.CfControl;
+import org.activiti.engine.task.Task;
+
 /**
  * @author rafizan.baharum
  * @since 10/3/13
  */
-public class ControlTask {
+public class ControlTask extends TaskAdapter {
+
+    private CfControl control;
+
+    public ControlTask(Task task, CfControl control) {
+        super(task);
+        this.control = control;
+    }
 }
