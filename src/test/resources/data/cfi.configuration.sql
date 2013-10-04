4c37f5785ca255f868243cf087364f2c1c830a04
Mohd Alif Haikal Abd Razak <alifhaikal88@gmail.com>
2013-10-05 01:27:53 +0800

    fixed date format

--- src/test/resources/data/cfi.configuration.sql
+++ src/test/resources/data/cfi.configuration.sql
@@ -1,12 +1,12 @@
 -- local, development or production?
-INSERT INTO FS_CONFIGURATION (ID, CONFIG_KEY, CONFIG_VALUE, DESCRIPTION, M_ST, C_ID, C_TS)
-VALUES(SEQ_FS_CONFIGURATION.NEXTVAL, 'CfConfiguration.environment', 'local', 'Environment key', 1, 0, SYSDATE);
+INSERT INTO CF_CFGN (ID, CONFIG_KEY, CONFIG_VALUE, DESCRIPTION, M_ST, C_ID, C_TS)
+  VALUES (nextval('seq_cf_cfgn'), 'CfConfiguration.environment', 'local', 'Environment key', 1, 0, CURRENT_TIMESTAMP);
 
-INSERT INTO FS_CONFIGURATION (ID, CONFIG_KEY, CONFIG_VALUE, DESCRIPTION, M_ST, C_ID, C_TS)
-VALUES(SEQ_FS_CONFIGURATION.NEXTVAL, 'CfConfiguration.email.enable', 'false', 'Email Configuration', 1, 0, SYSDATE);
+INSERT INTO CF_CFGN (ID, CONFIG_KEY, CONFIG_VALUE, DESCRIPTION, M_ST, C_ID, C_TS)
+  VALUES (nextval('seq_cf_cfgn'), 'CfConfiguration.email.enable', 'false', 'Email Configuration', 1, 0, CURRENT_TIMESTAMP);
 
-INSERT INTO FS_CONFIGURATION (ID, CONFIG_KEY, CONFIG_VALUE, DESCRIPTION, M_ST, C_ID, C_TS)
-VALUES(SEQ_FS_CONFIGURATION.NEXTVAL, 'CfConfiguration.email.contact', 'rafizan.baharum@gmail.com', 'Admin contact email', 1, 0, SYSDATE);
+INSERT INTO CF_CFGN (ID, CONFIG_KEY, CONFIG_VALUE, DESCRIPTION, M_ST, C_ID, C_TS)
+  VALUES (nextval('seq_cf_cfgn'), 'CfConfiguration.email.contact', 'rafizan.baharum@gmail.com', 'Admin contact email', 1, 0, CURRENT_TIMESTAMP);
 
-INSERT INTO FS_CONFIGURATION (ID, CONFIG_KEY, CONFIG_VALUE, DESCRIPTION, M_ST, C_ID, C_TS)
-VALUES(SEQ_FS_CONFIGURATION.NEXTVAL, 'CfConfiguration.reportServer', 'http://report/', 'Report Server URL', 1, 0, SYSDATE);
+INSERT INTO CF_CFGN (ID, CONFIG_KEY, CONFIG_VALUE, DESCRIPTION, M_ST, C_ID, C_TS)
+  VALUES (nextval('seq_cf_cfgn'), 'CfConfiguration.reportServer', 'http://report/', 'Report Server URL', 1, 0, CURRENT_TIMESTAMP);
